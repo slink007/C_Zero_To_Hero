53c168e18d56965597ec58c6b26acf93cf5cb627
slink007 <stephen.link0@gmail.com>
2025-07-05 09:23:22 -0400

    Remove duplication in file.c inherited from base project.

--- src/file.c
+++ src/file.c
@@ -9,32 +9,24 @@
 #include "common.h"
 
 
-int create_db_file(char *filename) {
-	int fd = open(filename, O_RDWR);
-	//close(fd);
-	if (fd != -1) {
-		close(fd);
-		printf("File already exists\n");
-		return STATUS_ERROR;
-	}
-
-	fd = open(filename, O_RDWR | O_CREAT, 0644);
+int test_fd(int fd) {
 	if (fd == -1) {
 		perror("open");
+		close(fd);
 		return STATUS_ERROR;
 	}
-
 	return fd;
 }
+
+
+int create_db_file(char *filename) {
+	int fd = open(filename, O_RDWR | O_CREAT, 0644);
+	return test_fd(fd);
+}
+
 
 int open_db_file(char *filename) {
-	int fd = open(filename, O_RDWR, 0644);  // permission levels needed, or even valid, here?
-	if (fd == -1) {
-		perror("open");
-		return STATUS_ERROR;
-	}
-
-	return fd;
+	int fd = open(filename, O_RDWR, 0644);
+	return test_fd(fd);
 }
-
 
